316acf904e811fd587503e0d3c62b38ef7305be5
Nicholas Pellizer <nicholasp21@gmail.com>
2025-04-14 14:18:23 +0200

    feat: footer with icons

--- src/data/site-info.ts
+++ src/data/site-info.ts
@@ -1,3 +1,10 @@
+import github from "../assets/github.svg";
+import instagram from "../assets/instagram.svg";
+import spotify from "../assets/spotify.svg";
+import applepodcasts from "../assets/applepodcasts.svg";
+import amazonmusics from "../assets/amazonmusic.svg";
+import type { ImageMetadata } from "astro";
+
 export const siteInfo = {
   title: "Oggi fa bora - il podcast su Trieste",
   description:
@@ -6,22 +13,39 @@ export const siteInfo = {
   url: "https://www.oggifabora.it",
 };
 
-export const socialLinks = [
-  { name: "Instagram", url: "https://www.instagram.com/oggifabora.podcast" },
-  { name: "GitHub", url: "https://github.com/Guybrush21/oggifabora" },
+export interface ExternalLink {
+  name: string;
+  url: string;
+  logo?: ImageMetadata;
+}
+
+export const socialLinks: ExternalLink[] = [
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/oggifabora.podcast",
+    logo: instagram,
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/Guybrush21/oggifabora",
+    logo: github,
+  },
 ];
 
-export const podcastLinks = [
+export const podcastLinks: ExternalLink[] = [
   {
     name: "Spotify",
     url: "https://open.spotify.com/show/21XphXWp2fuYShhpgzs6pN",
+    logo: spotify,
   },
   {
     name: "Amazon Music",
     url: "https://music.amazon.it/podcasts/5a45d986-b520-49a8-90dd-bc7f5135c656/oggi-fa-bora",
+    logo: amazonmusics,
   },
   {
     name: "Apple Podcasts",
     url: "https://podcasts.apple.com/us/podcast/oggi-fa-bora/id1806491928",
+    logo: applepodcasts,
   },
 ];
